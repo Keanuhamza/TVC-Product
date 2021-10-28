c5dfa534dcf7fb657877a3608b7522816d2735ff
Or1cle <kaibudic@gmail.com>
2021-10-28 20:56:50 +1100

    Fixed product equls()

--- src/main/java/com/example/ASWS/LoadDatabase.java
+++ src/main/java/com/example/ASWS/LoadDatabase.java
@@ -18,8 +18,21 @@ public class LoadDatabase {
   @Bean @Order
   CommandLineRunner initDatabase(ProductRepository repository, ProductDetailRepository repository2) {
     return args -> {
-      log.info("Preloading " + repository.save(new Product(1L, "Tools", "Hammer", 50f, 10)));
+      log.info("Preloading " + repository.save(new Product(1L, "Tools", "Hammer", 50f, 100)));
       log.info("Preloading " + repository2.save(new ProductDetail(1L, "Steel copper alloy", "Very popular")));
+
+      log.info("Preloading " + repository.save(new Product(2L, "Tools", "Spanner", 25f, 150)));
+      log.info("Preloading " + repository2.save(new ProductDetail(2L, "Steel alloy", "Very popular")));
+
+      log.info("Preloading " + repository.save(new Product(3L, "Food", "Cake", 31.50f, 190)));
+      log.info("Preloading " + repository2.save(new ProductDetail(3L, "Sweet", "Very popular")));
+
+      log.info("Preloading " + repository.save(new Product(4L, "Food", "Apple", 12.58f, 80)));
+      log.info("Preloading " + repository2.save(new ProductDetail(4L, "Red Fruit", "Very popular")));
+
+      log.info("Preloading " + repository.save(new Product(5L, "Food", "Orange", 96.10f, 79)));
+      log.info("Preloading " + repository2.save(new ProductDetail(5L, "Orange Fruit", "Very popular")));
+
     };
   } 
 
